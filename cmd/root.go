c51b1a8efc626c41c28d7274e7834ec400d0c08b
kPherox <admin@mail.kr-kp.com>
2019-04-20 09:08:43 +0900

    Remove `os.Exit`

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,6 @@ package cmd
 
 import (
     "fmt"
-    "os"
     "strings"
 
     homedir "github.com/mitchellh/go-homedir"
@@ -57,10 +56,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+    rootCmd.Execute()
 }
 
 var aliasFlags AliasFlags = map[string]string{
@@ -91,7 +87,7 @@ func initConfig() {
             home, err := homedir.Dir()
             if err != nil {
                 fmt.Println(err)
-                os.Exit(1)
+                return
             }
             cfgPath = home
         }
